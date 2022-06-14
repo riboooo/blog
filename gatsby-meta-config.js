997f60d292fa6be4797d4e168f4e831f7ca633e6
junyoung choi <junyeong.b2n2w@gmail.com>
2022-06-14 14:31:38 +0900

    ga 추가

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -27,6 +27,6 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '0', // Add your google analytics tranking ID
+  ga: 'G-BV5W6NL8DF', // Add your google analytics tranking ID
   ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
 }
